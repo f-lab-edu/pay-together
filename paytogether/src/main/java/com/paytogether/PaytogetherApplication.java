a603d257fd4717cc7e425d104822529b1d38df3a
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-19 15:19:15 +0900

    Feat #1 : 프로젝트 초기화

---
+++ paytogether/src/main/java/com/paytogether/PaytogetherApplication.java
@@ -0,0 +1,13 @@
+package com.paytogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytogetherApplication.class, args);
+    }
+
+}
