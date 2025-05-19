281971fc3930634dd18c1fb5661284da66d9d963
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-19 15:28:43 +0900

    Test #1 : 테스트 컨트롤러 생성

---
+++ paytogether/src/main/java/com/paytogether/TestController.java
@@ -0,0 +1,13 @@
+package com.paytogether;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testMethod() {
+        return "Hello World";
+    }
+}
