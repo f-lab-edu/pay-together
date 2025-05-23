d32af7d357a6ea8168df5b0051429bcdac450ce9
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-23 20:37:28 +0900

    feat: 회원 엔티티 구현
    - 회원 정보 컬럼 및 생성자 구현
    - 성별, 회원역할 Enum 클래스 구현
    
    Resolves: #3

---
+++ paytogether/src/main/java/com/paytogether/domain/member/entity/Member.java
@@ -0,0 +1,71 @@
+package com.paytogether.domain.member.entity;
+
+import com.paytogether.domain.member.service.MemberJoinRequest;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_id")
+  private Long id;
+
+  private String email;
+
+  private String password;
+
+  private String name;
+
+  private int age;
+
+  @Enumerated(EnumType.STRING)
+  private Gender gender;
+
+  private String address;
+
+  private String phoneNumber;
+
+  @Enumerated(EnumType.STRING)
+  private MemberRole role;
+  
+  private boolean isActive = true;
+
+  @Builder
+  private Member(String email, String password, String name, int age, Gender gender, String address,
+      String phoneNumber, MemberRole role) {
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.role = role;
+  }
+
+  public static Member createMember(MemberJoinRequest request) {
+    return Member.builder()
+        .email(request.getEmail())
+        .password(request.getPassword())
+        .name(request.getName())
+        .age(request.getAge())
+        .gender(request.getGender())
+        .address(request.getAddress())
+        .phoneNumber(request.getPhoneNumber())
+        .role(MemberRole.MEMBER)
+        .build();
+  }
+}
