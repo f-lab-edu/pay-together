db0ec4249cb5c75a79813808905864bd232ace56
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-24 17:06:50 +0900

    test: findByEmail 테스트
    Resolves: #3

--- paytogether/src/main/java/com/paytogether/domain/member/repository/MemberRepository.java
+++ paytogether/src/main/java/com/paytogether/domain/member/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.paytogether.domain.member.repository;
 
 import com.paytogether.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+  Optional<Member> findByEmail(String email);
 }
