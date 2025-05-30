06d64a39f8671c1e4ea624e469c4b8b2a915dfe4
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-30 22:43:27 +0900

    test: SoftAssertions 적용
    - 테스트 가독성을 위해 적용
    
    Resolves: #3

--- paytogether/src/test/java/com/paytogether/domain/member/service/MemberJoinRequestValidationTest.java
+++ paytogether/src/test/java/com/paytogether/domain/member/service/MemberJoinRequestValidationTest.java
@@ -7,6 +7,7 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import java.util.Set;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -41,6 +42,7 @@ class MemberJoinRequestValidationTest {
 
   @Test
   void validEmailBlank_Exception() {
+    //given
     MemberJoinRequest request = MemberJoinRequest.builder()
         .email("")
         .phoneNumber("0101234567")
@@ -48,17 +50,22 @@ class MemberJoinRequestValidationTest {
         .password("012345!67")
         .build();
 
+    //when
     Set<ConstraintViolation<MemberJoinRequest>> violations = validator.validate(request);
 
-    assertThat(violations).hasSize(1);
-    assertThat(violations)
+    //then
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(violations).hasSize(1);
+    softly.assertThat(violations)
         .extracting("message")
         .contains("공백일 수 없습니다");
+    softly.assertAll();
   }
 
   @ValueSource(strings = {"pay", "pay@", "@pay.com"})
   @ParameterizedTest
   void validEmailForm_Exception(String invalidEmail) {
+    //given
     MemberJoinRequest request = MemberJoinRequest.builder()
         .email(invalidEmail)
         .phoneNumber("0101234567")
@@ -66,23 +73,28 @@ class MemberJoinRequestValidationTest {
         .password("012345!67")
         .build();
 
+    //when
     Set<ConstraintViolation<MemberJoinRequest>> violations = validator.validate(request);
 
-    assertThat(violations).hasSize(1);
-    assertThat(violations)
+    //then
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(violations).hasSize(1);
+    softly.assertThat(violations)
         .extracting("message")
-        .contains("이메일 형식에 맞게 작성해주세요.");
+        .contains("공백일 수 없습니다");
+    softly.assertAll();
   }
 
-  @ParameterizedTest
+
   @ValueSource(strings = {
       "short1!", // 8자 미만
       "NoSpecial1", // 특수문자 없음
       "NoNumber!", // 숫자 없음
       "verylongpassword1234567890!" // 20자 초과
   })
+  @ParameterizedTest
   void validPasswordPattern_Exception(String invalidPassword) {
-    // given
+    //given
     MemberJoinRequest request = MemberJoinRequest.builder()
         .email(invalidPassword)
         .phoneNumber("0101234567")
@@ -90,8 +102,10 @@ class MemberJoinRequestValidationTest {
         .password("012345!67")
         .build();
 
+    //when
     Set<ConstraintViolation<MemberJoinRequest>> violations = validator.validate(request);
-    
+
+    //then
     assertThat(violations).hasSize(1);
   }
 }
