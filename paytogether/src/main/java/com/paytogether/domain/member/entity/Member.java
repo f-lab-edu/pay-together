f34861849f70480e8200ece498f639186b465e68
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-31 14:24:39 +0900

    feat: 공통 필드를 위한 BaseEntity 클래스
    - 생성날짜, 수정날짜 관리를 위해 적용
    
    Resolves: #4

--- paytogether/src/main/java/com/paytogether/domain/member/entity/Member.java
+++ paytogether/src/main/java/com/paytogether/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.paytogether.domain.member.entity;
 
+import com.paytogether.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Member {
+public class Member extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
