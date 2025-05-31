76c859e6af719a4550fb41aba1da9214c545eabc
tlqkrus012345 <tlqkrus012345@naver.com>
2025-05-31 14:25:23 +0900

    test: JpaAuditing
    - 생성날짜와 수정날짜 삽입 확인을 위해 구현
    
    Resolves: #4

---
+++ paytogether/src/test/java/com/paytogether/common/JpaAuditingTest.java
@@ -0,0 +1,32 @@
+package com.paytogether.common;
+
+import com.paytogether.domain.member.entity.Gender;
+import com.paytogether.domain.member.entity.Member;
+import com.paytogether.domain.member.repository.MemberRepository;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAuditingTest {
+
+  @Autowired
+  MemberRepository memberRepository;
+
+  @Test
+  void auditingTest() {
+    //given
+    Member member = Member.createMember("email@spring.com", "123456!6", "pay", 100, Gender.MALE,
+        "asdad", "01040121234");
+
+    //when
+    memberRepository.save(member);
+
+    //then
+    SoftAssertions softAssertions = new SoftAssertions();
+    softAssertions.assertThat(member.getCreatedAt()).isNotNull();
+    softAssertions.assertThat(member.getModifiedAt()).isNotNull();
+    softAssertions.assertAll();
+  }
+}
